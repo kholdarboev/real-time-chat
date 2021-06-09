5c066e98a5b58d788f3c49b1560e7c0108620119
sardorbek-sf <sardor4ik2013@gmail.com>
2021-06-08 18:25:37 -0700

    material ui used

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,23 @@
+import { AppBar, Toolbar, Button, Grid } from '@material-ui/core'
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../../utils'
+export default function Navbar() {
+    const user = false;
 
-export default function index() {
     return (
-        <div>
-
-        </div>
+        <AppBar style={{ background: "linear-gradient(-45deg, #39249a, #e14e42)" }} position="static">
+            <Toolbar variant='dense'>
+                <h3 >Sardorbek Kholdarboev</h3>
+                <Grid container justify={'flex-end'}>
+                    {user ? (<Button variant={'outlined'}>Logout</Button>)
+                        :
+                        <NavLink to={LOGIN_ROUTE}>
+                            (<Button variant={'outlined'}>Log In</Button>)
+                        </NavLink>
+                    }
+                </Grid>
+            </Toolbar>
+        </AppBar>
     )
 }
